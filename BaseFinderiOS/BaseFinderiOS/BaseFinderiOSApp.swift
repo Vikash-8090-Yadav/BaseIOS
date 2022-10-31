f04d123991f547c5cd4b303964976df2bd47dbd9
Vikash <yadav621gzp@gmail.com>
2022-10-31 20:48:24 +0530

    added the ios filews

---
+++ BaseFinderiOS/BaseFinderiOS/BaseFinderiOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  BaseFinderiOSApp.swift
+//  BaseFinderiOS
+//
+//  Created by Antoine Bollengier on 20.10.22.
+//
+
+import SwiftUI
+
+@main
+struct BaseFinderiOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
